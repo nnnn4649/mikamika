b87b88871ee8a200b3bc408b5d4377250805ee2d
nnnn4649 <nakagawa.nihonest@gmail.com>
2022-07-11 06:47:43 +0000

    fifth

--- mikamika/urls.py
+++ mikamika/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('index/', views.IndexView.as_view(), name='index'),
     path('mikamika/create/', views.MikamikaCreateView.as_view(), name='mikamika_create'),
     path('mikamika/create/complete/', views.MikamikaCreateCompleteView.as_view(), name='mikamika_create_complete'),
+    path('mikamika/list/', views.MikamikaListView.as_view(), name='mikamika_list'), # 追記
+    path('mikamika/detail/<uuid:pk>/', views.MikamikaDetailView.as_view(), name='mikamika_detail'), # 追記
 ]
--- mikamika/views.py
+++ mikamika/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import TemplateView
 from django.views.generic import CreateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
 from django.urls import reverse_lazy
 from .forms import MikamikaForm
+from .models import Mikamika
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -13,3 +16,11 @@ class MikamikaCreateView(CreateView):
 
 class MikamikaCreateCompleteView(TemplateView):
     template_name = 'mikamika_create_complete.html'
+
+class MikamikaListView(ListView):
+    template_name = 'mikamika_list.html'
+    model = Mikamika
+
+class MikamikaDetailView(DetailView):
+    template_name = 'mikamika_detail.html'
+    model = Mikamika
