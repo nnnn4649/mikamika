17e33b3b24944e8cd01ace8b89a15018c520b1aa
nnnn4649 <nakagawa.nihonest@gmail.com>
2022-07-24 14:10:54 +0000

    m-ninety

--- mikamika/views.py
+++ mikamika/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse_lazy
 from .forms import MikamikaForm
 from .models import Mikamika
 from django.utils import timezone
+from django.shortcuts import render
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -16,8 +17,13 @@ class FaView(CreateView):
     template_name = 'fa.html'
     form_class = MikamikaForm
     model = Mikamika
-    store = Mikamika.objects.filter(hyouka='good')
+
+    def mikamika_create(request):
+      store = Mikamika.objects.filter(hyouka='0').order_by('created_at').first()
+      store_list = Mikamika.objects.all()
+      return render(request, 'fa.html', {'store_list':store_list})
     success_url = reverse_lazy('mikamika:mikamika_create_complete')
+    
 
 class MikamikaCreateCompleteView(TemplateView):
     template_name = 'mikamika_create_complete.html'
