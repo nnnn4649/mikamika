89b9b962946b6dde3dde376f1dc0b439be03ffea
nnnn4649 <nakagawa.nihonest@gmail.com>
2022-10-12 13:56:35 +0000

    m-37

---
+++ env/djangoSampleApp/wsgi.py
@@ -0,0 +1,25 @@
+"""
+WSGI config for djangoSampleApp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+import sys 
+
+from django.core.wsgi import get_wsgi_application
+
+#path = '/var/www/html/djangoSampleApp'
+
+#if path not in sys.path:
+ #   sys.path.append(path)
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+#sys.path.append('/home/ec2-user/mikamika-1/env/djangoSampleApp')   #### 追加 ####
+#sys.path.append('/home/ec2-user/mikamika-1/env/djangoSampleApp/wsgi.py')   #### 追加 ####
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoSampleApp.settings')
+
+application = get_wsgi_application()
