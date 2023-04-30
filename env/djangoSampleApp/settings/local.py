265e3f01e5d67f8da2706304cf9fa11628fd3307
nnnn4649 <nakagawa.nihonest@gmail.com>
2023-04-30 17:14:32 +0000

    aws4

--- env/djangoSampleApp/settings/local.py
+++ env/djangoSampleApp/settings/local.py
@@ -1,29 +1,38 @@
 from .base import *
 
 
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
 
-#INSTALLED_APPS += [
-#    'debug_toolbar',
-#]
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, "static"),
-#)
+
+#DATABASES = {
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+   #     'ENGINE': 'django.db.backends.mysql',#追加
+  #      'NAME': 'mkmkdb',  #追加 db名
+   #     'USER': 'root',#追加
+   #     'PASSWORD': '3349',#追加
+       # 'HOST': '127.0.0.1',#追加
+  #      'HOST': 'localhost',#追加       
+  #      'PORT': '3306',#追加
+ #   }
+#}
 
 DATABASES = {
     'default': {
    #     'ENGINE': 'django.db.backends.sqlite3',
    #     'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',#追加
-        'NAME': 'mkmkdb',  #追加 db名
+        'NAME': 'mkdb',  #追加 db名
         'USER': 'root',#追加
-        'PASSWORD': '3349',#追加
-       # 'HOST': '127.0.0.1',#追加
-        'HOST': 'localhost',#追加       
+        'PASSWORD': '0<w<r?FHaFql123',#追加
+        'HOST': 'localhost',#追加
         'PORT': '3306',#追加
     }
 }
 
-INTERNAL_IPS = ['127.0.0.1']
+#INTERNAL_IPS = ['127.0.0.1']
+ALLOWED_HOSTS = ["mikamika.tk"]
